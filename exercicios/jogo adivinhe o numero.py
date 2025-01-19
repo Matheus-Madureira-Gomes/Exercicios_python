aca491a90ed35cd2217592561109f913603e2300
Matheus Madureira Gomes <matheus.madsep@gmail.com>
2025-01-19 03:15:31 -0300

    Update jogo adivinhe o numero.py

--- exercicios/jogo adivinhe o numero.py
+++ exercicios/jogo adivinhe o numero.py
@@ -1,7 +1,7 @@
 import random
 numero = random.randint(1,100)
 
-chute = int(input("Chute um número de 1 a 100"))
+chute = int(input("Chute um número de 1 a 100 "))
 
 while numero != chute:
     print("Você errou")
@@ -9,5 +9,5 @@ while numero != chute:
         print("O numero é maior")
     else:
         print("O número é menor")
-    chute = int(input("Chute um numero de 1 a 100"))
+    chute = int(input("Chute um numero de 1 a 100 "))
 print("Boa, você acertou!")
